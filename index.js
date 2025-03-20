0a042025c64fc3dfa5dd8d2757419ab253594c10
DivyaaSH23 <divyanshshuklaofficial23@gmail.com>
2025-03-20 17:04:21 +0530

    Ability to find  number of contacts in the address book

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const AddressBook = require("./ScriptJS/AddressBook");
 
 const myAddressBook = new AddressBook();
 
-const contact1 = new Contact("Ayushi", "Singh", "123 MG Road", "Delhi", "Delhi", "110001", "9876543210", "ayushi@example.com");
-const contact2 = new Contact("Ashutosh", "Sharma", "456 Lajpat Nagar", "Mumbai", "Maharashtra", "400001", "9876543211", "ashutosh@example.com");
+const contact1 = new Contact("Divyansh", "Shukla", "123 MG Road", "Delhi", "Delhi", "110001", "9876543210", "Divyansh@example.com");
+const contact2 = new Contact("Vivek", "Mishra", "456 Lajpat Nagar", "Mumbai", "Maharashtra", "400001", "9876543211", "ashutosh@example.com");
 
 myAddressBook.addContact(contact1);
 myAddressBook.addContact(contact2);
 
 myAddressBook.displayContacts();
 
-// Deleting Contact
-myAddressBook.deleteContact("Ayushi");
-
-myAddressBook.displayContacts();
+// Count Contacts using reduce()
+myAddressBook.countContacts();
